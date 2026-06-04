739ea4ee0e503950e02ffdc9e61ca3964f630b69
Lucas <tomzbraga@gmail.com>
2026-06-04 19:05:19 -0300

    adiciona o layout

---
+++ src/layout/layout.jsx
@@ -0,0 +1,23 @@
+import { Outlet } from "react-router";
+import { useEffect } from "react";
+import { useAuth } from "../contexts/AuthContext";
+import { useNavigate } from "react-router";
+
+function Layout() {
+  const { logado } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!logado) {
+      navigate("/login");
+    }
+  }, [logado, navigate]);
+
+  return (
+    <main>
+      <Outlet />
+    </main>
+  );
+}
+
+export default Layout;
