d7f8268c8506e61ae270ba0d11f6de1f5b90a9f4
Lucas <tomzbraga@gmail.com>
2026-06-05 18:59:02 -0300

    adiciona funcao de listagem de detalhes de filme especifico

---
+++ src/service/Service.js
@@ -0,0 +1,74 @@
+import axios from "axios";
+
+/* 
+
+PARAMETROS DA API 
+
+Para acessar o que a API retorna recomendo que olhe a URL comentada no topo de cada funcao
+
+exemplo:
+
+no componente Novidades.jsx eu coloquei um console.log({ movies }) os parametros podem ser usados como:
+
+movies.title
+movies.id
+movies.poster_path
+
+*/
+
+// https://developer.themoviedb.org/reference/movie-now-playing-list
+export async function getNovidades() {
+  try {
+    const response = await axios({
+      method: "get",
+      url: "https://api.themoviedb.org/3/movie/now_playing",
+      params: {
+        api_key: "a03dc348a47fa57d814f8290f647ee2c",
+        language: "pt-BR",
+      },
+    });
+
+    return response.data;
+  } catch (error) {
+    console.error("Erro ao carregar filmes:", error);
+    throw error;
+  }
+}
+
+// https://developer.themoviedb.org/reference/movie-popular-list
+export async function getPopulares() {
+  try {
+    const response = await axios({
+      method: 'get',
+      url: 'https://api.themoviedb.org/3/movie/popular',
+      params: {
+        api_key: "a03dc348a47fa57d814f8290f647ee2c",
+        language: "pt-BR"
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Erro ao carregar populares", error);
+    throw error;
+  }
+}
+
+// https://developer.themoviedb.org/reference/movie-details
+export async function getMovieDetails() {
+  try {
+    const response = await axios({
+      method: 'get',
+      url: `https://api.themoviedb.org/3/movie/{movie_id}`,
+      params: {
+        api_key: "a03dc348a47fa57d814f8290f647ee2c",
+      language: "pt-BR"
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Erro ao carregar populares", error);
+    throw error;
+  }
+}
+
+
